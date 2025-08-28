0946ae0beeba3693d7a62f156bd35c64c4cb0363
Sondre Madland <118986568+SMadland@users.noreply.github.com>
2025-08-28 09:11:09 +0200

    Updated supabaseclient.tsx

--- src/supabaseclient.tsx
+++ src/supabaseclient.tsx
@@ -1,7 +1,7 @@
+// src/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://azmrpbfifkznhqbyftsg.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6bXJwYmZpZmt6bmhxYnlmdHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNTkxODgsImV4cCI6MjA3MDczNTE4OH0.QUVT27UGijBsWHMxE8Fgfg0MPSVdFRWcqMRqjz3GTaQ";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6bXJwYmZpZmt6bmhxYnlmdHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNTkxODgsImV4cCI6MjA3MDczNTE4OH0.QUVT27UGijBsWHMxE8Fgfg0MPSVdFRWcqMRqjz3GTaQ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
