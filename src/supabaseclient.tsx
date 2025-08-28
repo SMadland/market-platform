5a912a1ca3fc62e12e382325a63720027683fdd6
Sondre Madland <118986568+SMadland@users.noreply.github.com>
2025-08-28 09:32:33 +0200

    Deleted supabaseclient.tsx

--- src/supabaseclient.tsx
+++
@@ -1,7 +0,0 @@
-// src/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://azmrpbfifkznhqbyftsg.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6bXJwYmZpZmt6bmhxYnlmdHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUxNTkxODgsImV4cCI6MjA3MDczNTE4OH0.QUVT27UGijBsWHMxE8Fgfg0MPSVdFRWcqMRqjz3GTaQ';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
